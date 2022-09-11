8404ae29cb9a2ba825bbdca632508219539ebf7f
Luca Tartaggia <luca.tartaggia@eng.it>
2022-09-11 13:59:57 +0200

    UI FeedPost finita

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screens/add_post_screen.dart';
 import 'package:instagram_flutter/screens/home_screen.dart';
 
+import '../screens/feed_screen.dart';
+
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  Text('feed'),
+  FeedScreen(),
   Text('search'),
   AddPostScreen(),
   Text('notif'),
